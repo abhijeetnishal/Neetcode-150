370bc8eee54bbfe787b338659f520476ad07da3f
abhijeetnishal <abhijeetnishal@gmail.com>
2023-11-03 21:52:12 +0530

    Arrays & Hashing commit

---
+++ Arrays & Hashing/contains-duplicate.py
@@ -0,0 +1,69 @@
+''' 
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+
+Constraints:
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+'''
+
+# TC: O(n*n)
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        arrLen = len(nums)
+        for i_idx in range(0, arrLen):
+            for j_idx in range(i_idx + 1, arrLen):
+                if nums[i_idx] == nums[j_idx]:
+                    return True
+                
+
+        return False    
+
+# TC: O(n*log(n))
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        arrLen = len(nums)
+
+        nums.sort()
+
+        for idx in range(0, arrLen - 1):
+            if nums[idx] == nums[idx + 1]:
+                return True
+
+        return False 
+
+# TC: O(n) SC: O(n)
+class Solution:
+    # Using Dictionary(hashmap)
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        arrLen = len(nums)
+
+        freqDict = {}
+
+        for idx in range(0, arrLen):
+            if nums[idx] in freqDict:
+                return True
+
+            freqDict[nums[idx]] = 1
+
+        return False 
+
+    # Using set
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        arrLen = len(nums)
+
+        arrSet = set()
+
+        for idx in range(0, arrLen):
+            if nums[idx] in arrSet:
+                return True
+            arrSet.add(nums[idx])
+
+        return False 
